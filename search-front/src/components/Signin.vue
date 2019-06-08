2cd086c04a9aa711d4f9370a582d6cef613cddcc
wilbobraggins <wilbobraggins@gmail.com>
2019-06-08 20:52:13 +0000

    added signup.vue and routes

--- search-front/src/components/Signin.vue
+++ search-front/src/components/Signin.vue
@@ -1,29 +1,21 @@
 <template>
   <div class="max-w-sm m-auto my-8">
     <div class="border p-10 border-grey-light shadow rounded">
-      <h3 class="text-2xl mb-6 text-grey-darkest">Sign Up</h3>
-      <form @submit.prevent="signup">
+      <h3 class="text-2xl mb-6 text-grey-darkest">Sign In</h3>
+      <form @submit.prevent="signin">
         <div class="text-red" v-if="error">{{ error }}</div>
 
         <div class="mb-6">
           <label for="email" class="label">E-mail Address</label>
-          <input type="email" v-model="email" class="input" id="email" placeholder="andy@web-crunch.com">
+          <input type="email" v-model="email" class="input" id="email" placeholder="123@example.com">
         </div>
-
         <div class="mb-6">
           <label for="password" class="label">Password</label>
           <input type="password" v-model="password" class="input" id="password" placeholder="Password">
         </div>
+        <button type="submit" class="font-sans font-bold px-4 rounded cursor-pointer no-underline bg-green hover:bg-green-dark block w-full py-4 text-white items-center justify-center">Sign In</button>
 
-        <div class="mb-6">
-          <label for="password_confirmation" class="label">Password Confirmation</label>
-          <input type="password" v-model="password_confirmation" class="input" id="password_confirmation" placeholder="Password Confirmation">
-        </div>
-        <button class="flex content-center border-blue-500 hover:border-red-500 ...">
-          Button
-        </button>
-
-        <div class="my-4"><router-link to="/" class="link-grey">Sign In</router-link></div>
+        <div class="my-4"><router-link to="/signup" class="link-grey">Sign up</router-link></div>
       </form>
     </div>
   </div>
@@ -31,30 +23,29 @@
 
 <script>
 export default {
-  name: 'Signup',
+  name: 'Signin',
   data () {
     return {
       email: '',
       password: '',
-      password_confirmation: '',
       error: ''
     }
   },
   created () {
-    this.checkedSignedIn()
+    this.checkSignedIn()
   },
   updated () {
-    this.checkedSignedIn()
+    this.checkSignedIn()
   },
   methods: {
-    signup () {
-      this.$http.plain.post('/signup', { email: this.email, password: this.password, password_confirmation: this.password_confirmation })
-        .then(response => this.signupSuccessful(response))
-        .catch(error => this.signupFailed(error))
+    signin () {
+      this.$http.plain.post('/signin', { email: this.email, password: this.password })
+        .then(response => this.signinSuccessful(response))
+        .catch(error => this.signinFailed(error))
     },
-    signupSuccessful (response) {
+    signinSuccessful (response) {
       if (!response.data.csrf) {
-        this.signupFailed(response)
+        this.signinFailed(response)
         return
       }
       localStorage.csrf = response.data.csrf
@@ -62,12 +53,12 @@ export default {
       this.error = ''
       this.$router.replace('/lookups')
     },
-    signupFailed (error) {
-      this.error = (error.response && error.response.data && error.response.data.error) || 'Something went wrong'
+    signinFailed (error) {
+      this.error = (error.response && error.response.data && error.response.data.error) || ''
       delete localStorage.csrf
       delete localStorage.signedIn
     },
-    checkedSignedIn () {
+    checkSignedIn () {
       if (localStorage.signedIn) {
         this.$router.replace('/lookups')
       }
--- search-front/src/router/index.js
+++ search-front/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Signin from '@/components/Signin.vue'
-
+import Signup from '@/components/Signup.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -11,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'Signin',
       component: Signin
+    },
+    {
+      path: '/signup',
+      name: 'Signup',
+      component: Signup
     }
   ]
 })

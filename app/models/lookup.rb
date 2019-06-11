9000dea8e516c338c56e7ef27f1fc6aa49e16718
wilbobraggins <wilbobraggins@gmail.com>
2019-06-11 20:30:15 +0000

    websearch and pull resaults methods

--- app/models/lookup.rb
+++ app/models/lookup.rb
@@ -1,5 +1,28 @@
 class Lookup < ApplicationRecord
   belongs_to :user
 
-  validates :search, presence: true
+  def self.websearch(input)
+    if input.scan(/\w+/).size <= 1
+      "https://stackoverflow.com/search?q=" + '/questions/tagged/' + "#{input}"
+    else
+      "https://stackoverflow.com/search?q=" + "#{input}".gsub!(/\s/,'+')
+    end
+  end
+
+  def self.pull_resaults(url)
+    page_unparsed = HTTParty.get(url)
+    page_parsed = Nokogiri::HTML(page_unparsed)
+    list = Array.new
+    listing = page_parsed.css('div.question-summary')
+
+    listing.each do |val|
+      res = {
+        title:        val.css('a.question-hyperlink'),
+        description:  val.css('div.excerpt'),
+        url:          'https://stackoverflow.com' + val.css('a')[0].attributes['href'].value
+      }
+    list << res
+    end
+  end
+
 end
